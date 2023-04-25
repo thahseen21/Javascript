14966fac9ccb7446cf1780735d7a0e7a9db3ce44
thahseen <thahseen.niaz@gmail.com>
2023-04-25 13:40:39 +0530

    find subset by recursion

--- subsetRecursion.js
+++ subsetRecursion.js
@@ -1,28 +1,16 @@
-let k = [4, 1, 8, 9];
-
-console.log(
-    k.filter((val, i) => {
-        if (i != 1) return val;
-    })
-);
-// let k = [1, 4, 6, 2];
-
-// let result = [];
+let k = [4, 1, 8];
 
 let finalResult = [];
 
 const take = (list, result, iteration) => {
     result.push(list[iteration]);
-    // console.log("take result", result, iteration);
     subset(list, result, ++iteration);
 };
 
 const leave = (list, result, iteration) => {
-    // result = result.slice(0, iteration);
     let potato = result.filter((val, i) => {
         if (i != iteration) return val;
     });
-    // console.log("leave", "result", result, "filtered", potato, iteration);
     subset(list, potato, ++iteration);
 };
 
@@ -30,7 +18,6 @@ const subset = (list, result, iteration = 0) => {
     if (iteration === list.length) {
         finalResult.push([...result]);
         result = [];
-        // console.log("existing subset", result, "------------", finalResult);
         return finalResult;
     }
     if (list.length > 0) {
@@ -42,5 +29,5 @@ const subset = (list, result, iteration = 0) => {
         return { result, list };
     }
 };
-subset(k, []);
-console.log("final potato ", finalResult, finalResult.length);
+let result = subset(k, []);
+console.log("result ", finalResult, finalResult.length);
